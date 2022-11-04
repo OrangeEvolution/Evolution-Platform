e275dfde5ffd689d4714bb0ef8373cba76056b9f
dukmarques <eduardomarques@outlook.com>
2022-11-04 12:58:19 -0300

    Tela de login e implementação de login via github

---
+++ pages/login.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+import styles from '../styles/Login.module.scss';
+import Image from 'next/image';
+
+export default function Login() {
+    const { data: session } = useSession();
+
+    return (
+        <div className={styles.container}>
+            {!session
+                ? <>
+                    <button onClick={(e) => { e.preventDefault(), signIn('github') }}>Login pelo GitHub</button>
+                    <button onClick={(e) => { e.preventDefault(), signIn('google') }}>Login pelo Google</button>
+                    <button onClick={(e) => { e.preventDefault(), signIn('facebook') }}>Login pelo Facebook</button>
+                </>
+                : <>
+                    <img className={styles.photo} src={session?.user?.image!} alt="Foto de Perfil" />
+                    Nome: {session.user?.name} <br />
+                    E-mail: {session.user?.email}
+                    <button onClick={(e) => { e.preventDefault(), signOut() }}>Deslogar</button>
+                </>
+            }
+        </div>
+    )
+}
