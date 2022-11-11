9cdfc34ea33332e155eeb5c28001270fe061b199
pablodamascenoo <pablofacex@gmail.com>
2022-11-11 16:33:49 -0300

    feat: finish footer

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,31 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <meta name="title" content="Orange Evolution" />
-                    <meta name="description" content="Explore conhecimentos que estão transformando indústrias, negócios e vidas através de trilhas gratuitas em Desenvolvimento, UX/UI Design e QA!" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="title" content="Orange Evolution" />
+          <meta
+            name="description"
+            content="Explore conhecimentos que estão transformando indústrias, negócios e vidas através de trilhas gratuitas em Desenvolvimento, UX/UI Design e QA!"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
