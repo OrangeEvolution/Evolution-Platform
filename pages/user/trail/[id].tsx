e6c6fadeceebb91f0230f55c80b02b0de39bc459
dukmarques <eduardomarques@outlook.com>
2022-11-14 23:40:14 -0300

    Modal com informações do conteúdo e botões para alterar status

--- pages/user/trail/[id].tsx
+++ pages/user/trail/[id].tsx
@@ -5,21 +5,30 @@ import { getSession, useSession } from "next-auth/react";
 import { findById, findFullTrailById, findFullTrailByIdAndProgress } from "../../../services/trails";
 import { Trail } from "../../../Types/Trail";
 
-import React from "react";
+import React, { useState } from "react";
 import { findAll } from "../../../services/contentType";
 import { findAll as findAllContent } from "../../../services/category";
-import { findAll as findAllProgress, findContentId } from "../../../services/contentProgress";
+import { findAll as findAllProgress, findContentId, updateProgress } from "../../../services/contentProgress";
 import Footer from '../../../components/Footer';
 import Link from 'next/link';
 import { ContentTpe } from '../../../Types/ContentType';
 import Head from 'next/head';
+import Modal from '../../../components/Modal';
+import { Content } from '../../../Types/Content';
+import { notifySuccess } from '../../../util/notifyToast';
+import { useRouter } from 'next/router';
 
 type TrailProps = {
   trail: any;
   contentsTypes: ContentTpe[];
 }
 export default function ContentDetails({ trail, contentsTypes }: TrailProps) {
+  const router = useRouter();
+  const pageTitle = `Trilha ${trail?.name} | Orange Evolution`;
+
   const { data: session } = useSession();
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [content, setContent] = useState<Content>();
 
   function getContentTypeName(id: number): string {
     let typeName = `${id}`;
@@ -46,14 +55,56 @@ export default function ContentDetails({ trail, contentsTypes }: TrailProps) {
     }
   }
 
+  function handleOpenModalContent(content: Content) {
+    setContent(content);
+    setOpenModal(true);
+  }
+
+  async function handleUpdateContentProgress(content: Content, progress: string) {
+    const res = await updateProgress(content.progress, {
+      content: content.id,
+      user: session?.user.id,
+      status: progress,
+      id: content.progress
+    }, session?.user.token);
+
+    notifySuccess(`Status de progresso no conteúdo alterado com sucesso.`);
+    setOpenModal(false);
+    router.reload();
+  }
+
   return (
     <>
       <div className={Styles.container}>
         <Head>
-          <title>Trilha {trail.name} | Orange Evolution</title>
+          <title>{pageTitle}</title>
         </Head>
 
         <Header />
+
+        <Modal
+          openModal={openModal}
+          closeModal={() => setOpenModal(false)}
+        >
+          <div className={Styles.modalContent}>
+            <h2>Informações sobre o conteúdo</h2>
+            <ul>
+              <li><strong>Título:</strong> <span>{content?.description}</span></li>
+              <li><strong>Tipo: </strong> <span>{getContentTypeName(content?.contentType)}</span></li>
+              <li><strong>Duração: </strong><span>{content?.durationInMinutes} minutos</span></li>
+              <li><strong>Status: </strong><span>{getProgressName(content?.progressEnum)}</span></li>
+              <li><strong>Conteúdo por: </strong><span>{content?.partner}</span></li>
+              <li>
+                <Link href={content?.link!} target="_blank">Acessar conteúdo</Link>
+              </li>
+              <li>
+                {content?.progressEnum === 'NOT_COMPLETED'
+                  ? <button onClick={(e) => { e.preventDefault(), handleUpdateContentProgress(content, 'CONCLUDED') }}>Alterar status para concluído</button>
+                  : <button onClick={(e) => { e.preventDefault(), handleUpdateContentProgress(content, 'NOT_COMPLETED') }} className={Styles.notCompleted}>Alterar status para não concluído</button>}
+              </li>
+            </ul>
+          </div>
+        </Modal>
 
         <div className={Styles.welcome}>
           <div className={Styles.user}>
@@ -73,7 +124,7 @@ export default function ContentDetails({ trail, contentsTypes }: TrailProps) {
               <ul>
                 <li>
                   {category.contents.map((content) => (
-                    <Link href={content.link} target='_blank' className={Styles.items} key={content.id}>
+                    <button className={Styles.items} key={content.id} onClick={(e) => { e.preventDefault(), handleOpenModalContent(content) }}>
                       <p><strong>{content.description}</strong></p>
                       <ul>
                         <li>{getContentTypeName(content.contentType)}</li>
@@ -81,7 +132,7 @@ export default function ContentDetails({ trail, contentsTypes }: TrailProps) {
                         <li>{getProgressName(content.progressEnum)}</li>
                         <li>{content.partner}</li>
                       </ul>
-                    </Link>
+                    </button>
                   ))}
                 </li>
               </ul>
@@ -98,6 +149,15 @@ export default function ContentDetails({ trail, contentsTypes }: TrailProps) {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
 
   const trail = await findFullTrailByIdAndProgress(context.query.id, session?.user.token);
   const contentType = await findAll(session?.user.token);
